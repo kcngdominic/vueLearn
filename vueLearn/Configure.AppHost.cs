095bcab80dadecb2e48d53b767a298cddfb8a249
kcngdominic <kcngdominic@gmail.com>
2023-12-28 12:07:12 +1000

    init

---
+++ vueLearn/Configure.AppHost.cs
@@ -0,0 +1,30 @@
+using Funq;
+using ServiceStack;
+using vueLearn.ServiceInterface;
+
+[assembly: HostingStartup(typeof(vueLearn.AppHost))]
+
+namespace vueLearn;
+
+public class AppHost : AppHostBase, IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+        });
+
+    public AppHost() : base("vueLearn", typeof(MyServices).Assembly) {}
+
+    public override void Configure(Container container)
+    {
+        // enable server-side rendering, see: https://sharpscript.net/docs/sharp-pages
+        Plugins.Add(new SharpPagesFeature {
+            EnableSpaFallback = true
+        }); 
+
+        SetConfig(new HostConfig
+        {
+            AddRedirectParamsToQueryString = true,
+        });
+    }
+}
